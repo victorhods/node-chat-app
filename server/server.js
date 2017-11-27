423da8851ddce7e66c04dcf5a4f85118693ed870
Victor Freitas <victor.freitas@tqi.com.br>
2017-11-27 13:52:04 -0200

    Add new message and create new message events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'mike',
+    text: 'Hey, What is going on.'
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('User was Disconnected');
   });
